5e2283d6fa7dd0a85f9d0ec7c1290c3af4224c88
AndyZang-Salz <kaiyuezang@gmail.com>
2021-02-23 14:35:25 +0800

    initial

---
+++ .idea/fileTemplates/internal/Python Script.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+---------------------------------------
+@file    : ${NAME}
+@Version : ??
+@Author  : Andy Zang
+@software: ${PRODUCT_NAME}
+@Project : BadApple_QuecPython
+@For     : 
+---------------------------------------
+"""
+
+# History:
+# ${DATE}: Create
+
+
+
+if __name__ == '__main__':
+    pass
